94fd066d86ed493abec75dc527c0f85715daf9db
lukeiscrazy <lukeweng020516@gmail.com>
2024-12-30 16:17:11 +0800

    Add files via upload

---
+++ routes/routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"awesomeProject/controllers"
+	"awesomeProject/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) {
+	api := r.Group("/api")
+	{
+		api.POST("/register", controllers.Register)
+		api.POST("/login", controllers.Login)
+
+		userGroup := api.Group("/user", middleware.AuthMiddleware())
+		{
+			userGroup.POST("/follow", controllers.FollowUser)
+			userGroup.DELETE("/unfollow", controllers.UnfollowUser)
+		}
+	}
+}
